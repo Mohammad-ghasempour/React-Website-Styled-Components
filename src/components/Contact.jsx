8e54482ddfa33772e01b06505ff902a9b89a5c9b
Mohammad Ghasempour <m.gh84nor@gmail.com>
2023-04-28 17:06:32 +0200

    contact page has been responsived

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,8 +3,7 @@ import styled from "styled-components";
 import LocationIcon from "../images/LocationIcon.png";
 import PhoneIcon from "../images/PhoneIcon.png";
 import PaperflyIcon from "../images/PaperFly.png";
-import BackgroundImage from "../images/background.jpg"
-
+import BackgroundImage from "../images/background.jpg";
 
 const Container = styled.div`
    height: 90%;
@@ -12,18 +11,37 @@ const Container = styled.div`
    background-size: cover;
    display: flex;
    align-items: center;
-`;
-
-const Title = styled.span`
-   font-size: 30px;
-   font-weight: bold;
+   @media only screen and (max-width: 430px) {
+      flex-direction: column;
+      justify-content: space-around;
+      align-items: center;
+           
+   }
 `;
 
 const Left = styled.div`
    width: 50%;
    margin-left: 50px;
    margin-top: 60px;
+   @media only screen and (max-width: 430px) {
+      width: 100%;
+      margin-left: 0px;
+      margin-top: 5px;
+      display: flex;
+      flex-direction: column;
+      justify-content: space-around;
+      align-items: center;
+   }
+   `;
+const Title = styled.span`
+   font-size: 30px;
+   font-weight: bold;
+   @media only screen and (max-width: 430px) {
+      text-align: center;
+      margin-bottom: 15px;
+   }
 `;
+
 const Form = styled.form`
    height: 300px;
    width: 500px;
@@ -31,18 +49,31 @@ const Form = styled.form`
    flex-direction: column;
    flex-wrap: wrap;
    margin-top: 20px;
+   @media only screen and (max-width: 430px) {
+      flex-wrap: nowrap;
+      margin-top: 5px;
+      width: 50%;
+   }
 `;
 const Input = styled.input`
    width: 200px;
    height: 20px;
    padding: 10px;
    margin: 15px 0px;
+   @media only screen and (max-width: 430px) {
+      margin: 5px 0px;
+      padding: 5px;
+   }
 `;
 const TextBox = styled.textarea`
    width: 200px;
    height: 90px;
    padding: 20px;
    margin: 15px 0px;
+   @media only screen and (max-width: 430px) {
+      margin: 5px 0px;
+      padding: 8px;
+   }
 `;
 const Button = styled.button`
    width: 240px;
@@ -53,28 +84,47 @@ const Button = styled.button`
    border-radius: 10px;
    padding: 10px;
    cursor: pointer;
+   @media only screen and (max-width: 430px) {
+      width: 220px;
+      padding: 5px;
+      margin-top: 10px;
+   }
 `;
 
 const Right = styled.div`
    width: 50%;
    display: flex;
    flex-direction: column;
+   @media only screen and (max-width: 430px) {
+      width: 100%;
+           
+   }
 `;
 
 const InfoContainer = styled.div`
    display: flex;
    align-items: center;
    margin-bottom: 10px;
-   `;
+   @media only screen and (max-width: 430px) {
+      margin-bottom: 0px;
+      height: 50px;;
+      margin-left: 20px;
+      align-items: center;
+   }
+`;
 const Icon = styled.img`
    width: 30px;
    height: 30px;
    padding: 10px;
-`;
+   `;
 const Text = styled.p`
    margin-left: 20px;
    font-size: 20px;
    color: #333;
+   @media only screen and (max-width: 430px) {
+      font-size: 18px;
+      margin-left: 10px;
+   }
 `;
 
 const Contact = () => {
@@ -96,13 +146,16 @@ const Contact = () => {
 
          <Right>
             <InfoContainer>
-               <Icon src={LocationIcon} ></Icon>    <Text>Stavanger, Rogaland State of Norway</Text>
+               <Icon src={LocationIcon}></Icon>{" "}
+               <Text>Stavanger, Rogaland State of Norway</Text>
             </InfoContainer>
             <InfoContainer>
-               <Icon src={PhoneIcon} /> <Text>+47 93 828 281</Text> <Text>+47 93 828 281</Text>
+               <Icon src={PhoneIcon} /> <Text>+47 93 828 281</Text>{" "}
+               <Text>+47 93 828 281</Text>
             </InfoContainer>
             <InfoContainer>
-               <Icon src={PaperflyIcon} /> <Text>Info@oxinweb.com</Text> <Text>M.gh84nor@gmail.com</Text>
+               <Icon src={PaperflyIcon} /> <Text>Info@oxinweb.com</Text>{" "}
+               <Text>M.gh84nor@gmail.com</Text>
             </InfoContainer>
          </Right>
       </Container>
